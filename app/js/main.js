66a47ea5db7f95ada003caf8abec9cbb56a86fe4
vladian95 <kvankvan1995@gmail.com>
2021-07-02 15:47:42 +0200

    start adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -11,5 +11,9 @@ $('.news__slider-inner').slick({
     nextArrow: '<button type="button" class="news__slick-btn news__slick-next"></button>',
     prevArrow: '<button type="button" class="news__slick-btn news__slick-prev"></button>'
 });
+
+$('.header__btn-menu').on('click', function(){
+    $('.menu ul').slideToggle();
+});
     
 });
